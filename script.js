3a3930d5833bf41ee0c169d4793d40722318c11f
pokan9713-source <pokan9713@gmail.com>
2026-07-23 23:56:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// FefeX Gaming animációk
 
+window.onload = function(){
+
+    console.log("FefeX Gaming oldal betöltve!");
+
+};
+
+
+// Kártyák animációja
+
+const cards = document.querySelectorAll(".card");
+
+
+cards.forEach(card => {
+
+    card.addEventListener("mouseenter",()=>{
+
+        card.style.transition="0.3s";
+
+    });
+
+
+});
